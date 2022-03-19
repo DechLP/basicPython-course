e7726203c538593adbba8474813d10084edf345d
DechLP <101938260+DechLP@users.noreply.github.com>
2022-03-19 22:35:35 +0700

    เต่าตะนุ

---
+++ ClassPythonW2.py
@@ -0,0 +1,10 @@
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def Rectangle():
+    for i in range(90):
+        tao.left(25)
+        tao.forward(250)
+        tao.left(90)
+
+Rectangle()
